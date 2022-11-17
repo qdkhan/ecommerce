ff8312e1fc4531a9f02f4326978792f6983859f2
qdkhan <qutbuddin.khan@xipetech.com>
2022-11-17 19:52:58 +0530

    fdff

---
+++ app/Http/Livewire/HeaderSearch.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Category;
+
+class HeaderSearch extends Component
+{
+    public $search, $product_cat, $product_cat_id;
+
+    public function mount() { 
+        $this->product_cat = 'All Category';
+        $this->fill(request()->only('search', 'product_cat', 'product_cat_id'));
+    }
+
+    public function render()
+    {
+        $categories = Category::all();
+        return view('partials.header-search', ['categories' => $categories]);
+    }
+}
---
+++ resources/views/partials/header-search.blade.php
@@ -0,0 +1,17 @@
+<div class="wrap-search center-section">
+    <div class="wrap-search-form">
+        <form action="#" id="form-search-top" name="form-search-top">
+            <input type="text" name="search" value="" placeholder="Search here...">
+            <button form="form-search-top" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+            <div class="wrap-list-cate">
+                <input type="hidden" name="product_cat" value="" id="product_cat">
+                <input type="hidden" name="product_cat_id" value="" id="product_cat_id">
+                <a href="#" class="link-control"></a>
+                <ul class="list-cate">
+                    <li class="level-0">All Category</li>
+                    @php echo(111) @endphp
+                </ul>
+            </div>
+        </form>
+    </div>
+</div>
