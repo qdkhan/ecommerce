ac02304694c555771003d4760e9cd4ac765b79a9
qdkhan <qutbuddin.khan@xipetech.com>
2022-11-22 17:37:10 +0530

    Search

--- resources/views/livewire/header-search.blade.php
+++ resources/views/livewire/header-search.blade.php
@@ -4,8 +4,8 @@
             <input type="text" name="search" value="" placeholder="Search here...">
             <button form="form-search-top" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
             <div class="wrap-list-cate">
-                <input type="hidden" name="product_cat" value="{{$product_cat}}" id="product_cat">
-                <input type="hidden" name="product_cat_id" value="{{$product_cat_id}}" id="product_cat_id">
+                <input type="hidden" name="product_cat" value="{{$product_cat}}" id="product-cat">
+                <input type="hidden" name="product_cat_id" value="{{$product_cat_id}}" id="product-cat-id">
                 <a href="#" class="link-control">{{str_split($product_cat,12)[0]}}</a>
                 <ul class="list-cate">
                     <li class="level-0">All Category</li>
